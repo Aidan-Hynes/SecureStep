0760033dc45b2c733ec00915ba45e8a907bd99f2
KordianM <kordianm03@gmail.com>
2024-09-14 21:55:35 -0400

    Our location test

--- src/components/DrawNavigation.js
+++ src/components/DrawNavigation.js
@@ -3,7 +3,7 @@ import { Navigation, useMap } from "@mappedin/react-sdk";
 export default function DrawNavigation() {
   const { mapData, mapView } = useMap();
 
-  const Coord = mapView.createCoordinate(43.473139, -80.539777);
+  const Coord = mapView.createCoordinate(43.472795, -80.5394083);
   const space = mapData
     .getByType("space")
     .filter((space) => space.floor.id === mapView.currentFloor.id)[10];
