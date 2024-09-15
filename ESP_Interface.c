462e8b65bb4271fa3ccc764508af77af4cd7104f
Aidan Hynes <ajphynes@uwaterloo.ca>
2024-09-14 20:03:49 -0400

    Add Google cloud API Geolocation

--- ESP_Interface.c
+++ ESP_Interface.c
@@ -1,39 +1,130 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
+#include <WiFiClientSecureBearSSL.h>  // For SSL/TLS
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 
+#define PIN_RED    14 
+#define PIN_GREEN  13
+#define PIN_BLUE   12
+
 Adafruit_MPU6050 mpu;
 sensors_event_t a, g, temp;
+String lat, lng;
 
 const char* ssid = "HackTheNorth";
-const char* password = "FAKE PASSWORD";
+const char* password = "HTNX2024";
 
-
+const char* apiKey = "AIzaSyAi2tTIYkHWwyQASN2nBa_6plmzPO1RkxA";
+const char* geolocationUrl = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAi2tTIYkHWwyQASN2nBa_6plmzPO1RkxA";
 
 // Create a web server on port 80
 ESP8266WebServer server(80);
 
-// This function will handle incoming HTTP requests
+
+//Set LED Light Functions
+void RedLED() {
+  digitalWrite(PIN_RED,   HIGH);
+  digitalWrite(PIN_GREEN, LOW);
+  digitalWrite(PIN_BLUE,  LOW);
+}
+
+void GreenLED() {
+  digitalWrite(PIN_RED,   LOW);
+  digitalWrite(PIN_GREEN, HIGH);
+  digitalWrite(PIN_BLUE,  LOW);
+}
+
+void BlueLED() {
+  digitalWrite(PIN_RED,   LOW);
+  digitalWrite(PIN_GREEN, LOW);
+  digitalWrite(PIN_BLUE,  HIGH);
+}
+
+void OffLED() {
+  digitalWrite(PIN_RED,   LOW);
+  digitalWrite(PIN_GREEN, LOW);
+  digitalWrite(PIN_BLUE,  LOW);
+}
+
+
+//This function will handle incoming HTTP requests
 void handleRoot() {
-  // Send JSON data with CORS headers
+  //Send JSON data
+  GreenLED();  
   String jsonAccel = "{\"Accel-X\" : " + String(a.acceleration.x) + "," + "\"Accel-Y\" : " + String(a.acceleration.y) + "," + "\"Accel-Z\" : " + String(a.acceleration.z) + "}";
   String jsonGyro = "{\"Gyro-X\" : " + String(g.gyro.x) + "," + "\"Gyro-Y\" : " + String(g.gyro.y) + "," + "\"Gyro-Z\" : " + String(g.gyro.z) + "}"; 
-
-  String jsonResponse = jsonAccel + "," + jsonGyro;
+  String jsonLoc = "{\"Lat\" : " + String(lat) + "," + "\"Lat\" : " + String(lng) + "}";
+  String jsonResponse = jsonAccel + "," + jsonGyro + "," + jsonLoc;
   Serial.println(jsonResponse);
   
   server.sendHeader("Access-Control-Allow-Origin", "*"); // Allow CORS
   server.send(200, "application/json", jsonResponse);
 }
 
+
+void getLocation() {
+  //Scan for geolocation points
+  Serial.println("Scanning for Wi-Fi networks...");
+  int n = WiFi.scanNetworks();  // Perform network scan
+
+  String jsonWifiPoints;
+  if (n == 0) {
+    Serial.println("No networks found");
+  } 
+  else {
+    Serial.print(n);
+    Serial.println(" networks found"); 
+    jsonWifiPoints = "{\"wifiAccessPoints\":[";
+    for (int i = 0; i < n; ++i) {
+      jsonWifiPoints += "{\"macAddress\":\"" + WiFi.BSSIDstr(i) + "\",";
+      jsonWifiPoints += "\"signalStrength\":" + String(WiFi.RSSI(i)) + "}";
+      if (i < n - 1) jsonWifiPoints += ",";
+    }
+    jsonWifiPoints += "]}";
+  }
+  Serial.println("JSON WiFi Payload: " + jsonWifiPoints);
+  WiFi.scanDelete();
+
+  //make SSL insecure lol
+  BearSSL::WiFiClientSecure client;
+  client.setInsecure();
+  HTTPClient https;
+  https.begin(client, geolocationUrl);
+  https.addHeader("Content-Type", "application/json");
+  
+  //Send payload;
+  int httpCode = https.POST(jsonWifiPoints);
+  
+  //Check the return value
+  if (httpCode > 0) {
+    //get the response
+    String payload = https.getString();
+    Serial.println("Response:");
+    Serial.println(payload);
+    lat = payload.substring(payload.indexOf("lat")+6, payload.indexOf(","));
+    lng = payload.substring(payload.indexOf("lng")+6, payload.indexOf("}"));
+  }
+  else {
+    Serial.print("Error on HTTP request: ");
+    Serial.println(httpCode);
+  }
+  
+  https.end(); //Close connection
+}
+
+
 void setup() {
+
+  pinMode(PIN_RED,   OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
+  pinMode(PIN_BLUE,  OUTPUT);  
+
   Serial.begin(115200);
 
-  while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+  while (!Serial) delay(10);
 
   Serial.println("Adafruit MPU6050 test!");
 
@@ -46,80 +137,33 @@ void setup() {
   }
   Serial.println("MPU6050 Found!");
 
-  //init accel
+  //init accel and gyro
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-    case MPU6050_RANGE_2_G:
-      Serial.println("+-2G");
-      break;
-    case MPU6050_RANGE_4_G:
-      Serial.println("+-4G");
-      break;
-    case MPU6050_RANGE_8_G:
-      Serial.println("+-8G");
-      break;
-    case MPU6050_RANGE_16_G:
-      Serial.println("+-16G");
-      break;
-  }
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-    case MPU6050_RANGE_250_DEG:
-      Serial.println("+- 250 deg/s");
-      break;
-    case MPU6050_RANGE_500_DEG:
-      Serial.println("+- 500 deg/s");
-      break;
-    case MPU6050_RANGE_1000_DEG:
-      Serial.println("+- 1000 deg/s");
-      break;
-    case MPU6050_RANGE_2000_DEG:
-      Serial.println("+- 2000 deg/s");
-      break;
-  }
-  //other stuff
   
+  //set filter
   mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-    case MPU6050_BAND_260_HZ:
-      Serial.println("260 Hz");
-      break;
-    case MPU6050_BAND_184_HZ:
-      Serial.println("184 Hz");
-      break;
-    case MPU6050_BAND_94_HZ:
-      Serial.println("94 Hz");
-      break;
-    case MPU6050_BAND_44_HZ:
-      Serial.println("44 Hz");
-      break;
-    case MPU6050_BAND_21_HZ:
-      Serial.println("21 Hz");
-      break;
-    case MPU6050_BAND_10_HZ:
-      Serial.println("10 Hz");
-      break;
-    case MPU6050_BAND_5_HZ:
-      Serial.println("5 Hz");
-      break;
-  }
+
   Serial.println("");
   delay(100);
 
-  //Wi-fi connection
+  //Wifi connection
   WiFi.begin(ssid, password);
 
-  // Wait for connection
+  //Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
     Serial.println("Connecting to WiFi...");
-  }
-  Serial.println("Connected to WiFi");
 
-  // Start the web serve
+    BlueLED();
+    delay(500);
+    OffLED();
+    delay(500);
+  }
+
+  Serial.println("Connected to WiFi"); 
+  BlueLED();
+
+  // Start the web server
   server.begin();
   server.on("/", handleRoot);
   String IP_Address = WiFi.localIP().toString();
@@ -127,45 +171,13 @@ void setup() {
   Serial.println("Web server started");
   Serial.print("ESP8266 IP Address: ");
   Serial.println(IP_Address);
-
-  // String jsonIP = "{\"IP Address\" : " + IP_Address + "}";
-  
-  // HTTPClient http;
-  // WiFiClient wifiClient;
-
-  // http.begin(wifiClient, "http://10.37.101.189:3000/");
-  // http.addHeader("Content-Type", "application/json");
-  // http.POST(jsonIP);
-  // http.end();
 }
+
 
 void loop() {
  //Get new sensor events with the readings
   mpu.getEvent(&a, &g, &temp);
 
   delay(10);
-
   server.handleClient();
-
-  Serial.println("Scanning for Wi-Fi networks...");
-  int n = WiFi.scanNetworks();  // Perform network scan
-  
-  if (n == 0) {
-    Serial.println("No networks found");
-  } 
-  else {
-    Serial.print(n);
-    Serial.println(" networks found");
-
-    for (int i = 0; i < n; ++i) {
-      // Print each network's SSID, RSSI, and BSSID (MAC address)
-      Serial.print("Network: ");
-      Serial.print(WiFi.SSID(i));
-      Serial.print(" (MAC: ");
-      Serial.print(WiFi.BSSIDstr(i));
-      Serial.print("), Signal Strength (RSSI): ");
-      Serial.print(WiFi.RSSI(i));
-      Serial.println(" dBm");
-    }
-  }    
 }
